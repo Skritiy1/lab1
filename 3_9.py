18eaac794381d9dff77a9b0d0fa40b8462de98a6
Skritiy1 <71488104+Skritiy1@users.noreply.github.com>
2024-09-20 15:32:07 +0300

    Update 3_9.py

--- 3_9.py
+++ 3_9.py
@@ -4,16 +4,19 @@ y = 0
 cur = 1
 s = 0
 
-while abs(cur) > 0.0001:
+for x in range(10,50,5):
+    x /= 100
     i = 0
-
-    for x in range(10,50,5):
-        x /= 100
-        cur = (-1)**i * (( x ** (2*i + 1) ) / 2 * i + 1)
-        y += atan(x)
-
-        s += cur
+    y = atan(x)
+    s = 0
+    cur = 10**20
+    while abs(cur) > 0.0001:
+        cur = ( (-1)**i)*((x**(2*i+1) ) / (2*i+1))
         i += 1
-    if cur < 0.0001:
-        print(s - cur, y - atan(x))
-        break
+        s += cur
+
+
+    print(s, y)
+
+
+
