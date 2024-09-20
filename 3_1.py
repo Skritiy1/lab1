3be33ce51d90f5f8b1a1e58efd26af211edbcd20
Skritiy1 <71488104+Skritiy1@users.noreply.github.com>
2024-09-20 15:33:29 +0300

    Update 3_1.py

--- 3_1.py
+++ 3_1.py
@@ -1,17 +1,21 @@
 from math import *
-cur=1
-s=0
+
 y = 0
-while abs(cur) > 0.0001:
+cur = 1
+s = 0
 
-     i = 0
-     for x in range(1, 11, 1):
-        x = x / 10
+for x in range(1, 11, 1):
+    x /= 10
+    i = 0
+    y = cos(x)
+    s = 0
+    cur = 10**20
+    while abs(cur) > 0.0001:
         cur = ( (-1)**i )*( (x**(2*i) ) / ( factorial(2*i) ) )
-
-        s += cur
-        y += cos(x)
         i += 1
-     if cur < 0.0001:
-         print(s - cur, y - cos(x))
-         break
+        s += cur
+
+
+    print(s, y)
+
+     
